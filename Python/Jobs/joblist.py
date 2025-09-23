22d2d92dbab070c09af582df0c4eb613579dd742
JoshMoore-Cohesity <josh.moore@cohesity.com>
2025-09-23 11:51:33 -0400

    Update joblist.py

--- Python/Jobs/joblist.py
+++ Python/Jobs/joblist.py
@@ -6,6 +6,8 @@
 # import pyhesity wrapper module
 from pyhesity import *
 import json
+from datetime import time, datetime, timedelta
+import codecs
 
 # command line arguments
 import argparse
@@ -20,7 +22,8 @@ parser.add_argument('-i', '--useApiKey', action='store_true')
 parser.add_argument('-pwd', '--password', type=str, default=None)
 parser.add_argument('-np', '--noprompt', action='store_true')
 parser.add_argument('-e', '--environment', type=str, default=None)
-
+parser.add_argument('-j', '--jobname', type=str, default=None)
+parser.add_argument('-paused', '--paused', action='store_true')
 
 args = parser.parse_args()
 
@@ -34,6 +37,12 @@ useApiKey = args.useApiKey
 password = args.password
 noprompt = args.noprompt
 environment = args.environment
+jobname = args.jobname
+ispaused = args.paused
+
+#Date
+now = datetime.now()
+dateString = now.strftime("%Y-%m-%d")
 
 # gather list function
 def gatherList(param=None, filename=None, name='items', required=True):
@@ -68,6 +77,26 @@ if apiconnected() is False:
     print('authentication failed')
     exit(1)
 
+#Create Outfile
+outfile = 'pg_summary-%s.csv' % dateString
+f = codecs.open(outfile, 'w')
+f.write('Cluster,Job Name,Paused,Start Time,Environment,Total Objects, AutoProtect Objects\n')
+
+report = []
+
+if clustername == ['all']:
+    clusternames = []
+    clusters = api('get', 'cluster-mgmt/info',mcmv2=True)
+    allclusters = clusters['cohesityClusters']
+    for cluster in allclusters:
+        if cluster['isConnectedToHelios'] == False:
+            print(cluster['clusterName'],"Not Connected to Helios")
+            report.append('%s,Disconnected From Helios' % cluster['clusterName'])
+            continue
+        clusternames.append(cluster['clusterName'])
+
+
+
 # if connected to helios or mcm, select access cluster
 for cluster in clusternames:
     if mcm or vip.lower() == 'helios.cohesity.com':
@@ -78,18 +107,36 @@ for cluster in clusternames:
         exit(1)
 # end authentication =====================================================
 
-jobs = api('get', 'data-protect/protection-groups?isActive=True', v=2)
-jobs = list(jobs.values())
+    jobs = api('get', 'data-protect/protection-groups?isActive=True&useCachedData=false', v=2)
+    jobs = jobs['protectionGroups']
+    if jobs is None:
+        print("No Active Jobs Found")
+        continue
 
-for job in jobs:
-    for item in job:
-        if environment is None or item['environment'][1:].lower() == environment.lower():
-            #print(item['name'])
-                if item['environment'] == 'kVMware':
-                    vmparams = item['vmwareParams']['objects']
-                    #vmparams = list(vmparams.values())
+    if ispaused:
+        jobs = [j for j in jobs if j['isPaused'] == True]
+
+    if jobname:
+        jobs = [j for j in jobs if jobname.lower() in j['name'].lower() ]
+
+    for job in jobs:
+        starttime = job['startTime']
+        starttimeobject = time(hour=starttime['hour'], minute=starttime['minute'])
+        paused = job['isPaused']
+        if 'numProtectedObjects' not in job:
+            job['numProtectedObjects'] = "NA"
+        if environment is None or job['environment'][1:].lower() == environment.lower():
+                if job['environment'] == 'kVMware':
+                    vmparams = job['vmwareParams']['objects']
                     vmautoprotecttotal = len(vmparams)
-                    #print(vmautoprotecttotal)
-                    print('%s (%s) (%s %s) (%s %s)' % (item['name'], item['environment'], 'Objects:', item['numProtectedObjects'],'Auto Protected:',vmautoprotecttotal))
+                    print('%s (Paused: %s) (%s) (%s) (%s %s) (%s %s)' % (job['name'],paused,starttimeobject,job['environment'], 'Objects:', job['numProtectedObjects'],'Auto Protected:',vmautoprotecttotal))
+                    report.append('%s,%s,%s,%s,%s,%s,%s' % (cluster,job['name'],paused,starttimeobject,job['environment'],job['numProtectedObjects'],vmautoprotecttotal))                
                 else:
-                    print('%s (%s) (%s %s)' % (item['name'],item['environment'], 'Objects:', item['numProtectedObjects']))
+                    print('%s (Paused: %s) (%s) (%s) (%s %s)' % (job['name'],paused,starttimeobject, job['environment'], 'Objects:', job['numProtectedObjects']))
+                    report.append('%s,%s,%s,%s,%s,%s' % (cluster,job['name'],paused,starttimeobject,job['environment'],job['numProtectedObjects']))
+
+for item in report:
+    f.write('%s\n' % item)
+
+f.close()
+print('\nOutput saved to %s\n' % outfile)
