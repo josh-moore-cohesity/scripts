7fb3b8965c7e9b1be3d3d8c7d36c1c21874f07fb
JoshMoore-Cohesity <josh.moore@cohesity.com>
2026-03-25 09:42:27 -0400

    Update import_pgs.py

--- Python/importExport/import_pgs.py
+++ Python/importExport/import_pgs.py
@@ -13,7 +13,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
 parser.add_argument('-u', '--username', type=str, default='helios')
 parser.add_argument('-d', '--domain', type=str, default='local')
-parser.add_argument('-c', '--clustername', nargs='+', type=str, default=None)
+parser.add_argument('-sc', '--clustername', nargs='+', type=str, default=None)
+parser.add_argument('-tc', '--targetclustername', type=str, default=None)
 parser.add_argument('-cl', '--clusters', type=str, default=None)
 parser.add_argument('-mcm', '--mcm', action='store_true')
 parser.add_argument('-i', '--useApiKey', action='store_true')
@@ -23,6 +24,8 @@ parser.add_argument('-m', '--mfacode', type=str, default=None)
 parser.add_argument('-e', '--emailmfacode', action='store_true')
 parser.add_argument('-newpass', '--newpassword', type=str, default=None)  # password for local user
 parser.add_argument('-o', '--outputpath', type=str, default='./configExports')
+parser.add_argument('-env', '--environment', type=str, default=None)
+parser.add_argument('-preview', '--preview', action='store_true')
 
 args = parser.parse_args()
 
@@ -30,6 +33,7 @@ vip = args.vip
 username = args.username
 domain = args.domain
 clustername = args.clustername
+targetclustername = args.targetclustername
 clusterlist = args.clusters
 mcm = args.mcm
 useApiKey = args.useApiKey
@@ -39,6 +43,8 @@ mfacode = args.mfacode
 emailmfacode = args.emailmfacode
 newpassword = args.newpassword
 outputpath = args.outputpath
+environment = args.environment
+preview = args.preview
 
 # gather server list
 def gatherList(param=None, filename=None, name='items', required=True):
@@ -86,7 +92,7 @@ for clustername in clusternames:
     print(clustername)
     # if connected to helios or mcm, select access cluster
     if mcm or vip.lower() == 'helios.cohesity.com':
-        heliosCluster(clustername)
+        heliosCluster(targetclustername)
     if LAST_API_ERROR() != 'OK':
         continue
     thisclusterpath = "%s/%s" % (outputpath,clustername)
@@ -97,14 +103,21 @@ for clustername in clusternames:
     with open(pgs_output_file, 'r') as file:
         pgs_payload = json.load(file)
 
+    
+
     currentpgs = api('get', 'data-protect/protection-groups?allUnderHierarchy=true', v=2)
     currentpgs = currentpgs['protectionGroups']
     
+
     for pg in pgs_payload['protectionGroups']:
+        if environment is not None:
+            if pg['environment'] != environment:
+                continue
         localpg= [p for p in currentpgs if p['name'].lower() == pg['name'].lower()]
         if len(localpg) == 0:
             print('Adding PG %s' % pg['name'])
-            newpg= api('post', 'data-protect/protection-groups', pg, v=2)
+            if not preview:
+                newpg= api('post', 'data-protect/protection-groups', pg, v=2)
 
         else:
             print('PG %s already exists...' % pg['name'])
