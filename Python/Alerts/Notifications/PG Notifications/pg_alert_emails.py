9e7c47b96de05b71e4319746658f536232c76d3c
JoshMoore-Cohesity <josh.moore@cohesity.com>
2026-07-27 11:24:46 -0400

    Update pg_alert_emails.py

--- Python/Alerts/Notifications/PG Notifications/pg_alert_emails.py
+++ Python/Alerts/Notifications/PG Notifications/pg_alert_emails.py
@@ -22,6 +22,9 @@ parser.add_argument('-np', '--noprompt', action='store_true')
 parser.add_argument('-m', '--mfacode', type=str, default=None)
 parser.add_argument('-e', '--emailmfacode', action='store_true')
 parser.add_argument('-outputpath', '--outputpath', type=str, default='./Results')
+parser.add_argument('-add', '--add', type=str, action='append', default=None)
+parser.add_argument('-remove', '--remove', type=str, action='append', default=None)
+parser.add_argument('-pglist', '--pglist', type=str, default=None)
 
 args = parser.parse_args()
 
@@ -37,6 +40,9 @@ noprompt = args.noprompt
 mfacode = args.mfacode
 emailmfacode = args.emailmfacode
 outputpath = args.outputpath
+addEmails = args.add or []
+removeEmails = args.remove or []
+pglistfile = args.pglist
 
 # gather server list
 def gatherList(param=None, filename=None, name='items', required=True):
@@ -53,8 +59,30 @@ def gatherList(param=None, filename=None, name='items', required=True):
         exit()
     return items
 
+# load csv of clustername,pgname pairs to restrict -add/-remove to
+def loadPgList(filename):
+    if filename is None:
+        return None
+    pairs = set()
+    with open(filename, newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if len(row) < 2 or row[0].strip() == '':
+                continue
+            pairs.add((row[0].strip().lower(), row[1].strip().lower()))
+    if len(pairs) == 0:
+        print('no protection groups found in %s' % filename)
+        exit()
+    return pairs
+
+pgList = loadPgList(pglistfile)
+
 # get list of clusters
 clusternames = gatherList(clustername, clusterlist, name='clusters', required=False)
+
+# if no clusters were explicitly given, narrow to the clusters referenced in -pglist
+if len(clusternames) == 0 and pgList is not None:
+    clusternames = sorted(set(pair[0] for pair in pgList))
 
 
 #Date and Time
@@ -104,8 +132,43 @@ for clustername in clusternames:
         alertPolicy = pg.get('alertPolicy') or {}
         alertTargets = alertPolicy.get('alertTargets', []) or []
         backupRunStatus = ','.join(alertPolicy.get('backupRunStatus', []) or [])
+
+        inScope = pgList is None or (clustername.lower(), pgname.lower()) in pgList
+        modifying = len(addEmails) > 0 or len(removeEmails) > 0
+        changed = False
+
+        if modifying and inScope:
+            if len(removeEmails) > 0:
+                keptTargets = [t for t in alertTargets if t.get('emailAddress') not in removeEmails]
+                if len(keptTargets) != len(alertTargets):
+                    changed = True
+                alertTargets = keptTargets
+
+            if len(addEmails) > 0:
+                existingEmails = [t.get('emailAddress') for t in alertTargets]
+                newTargets = [
+                    {
+                        "emailAddress": e,
+                        "language": "en-us",
+                        "recipientType": "kTo"
+                    }
+                    for e in addEmails
+                    if e not in existingEmails
+                ]
+                if len(newTargets) > 0:
+                    changed = True
+                alertTargets = alertTargets + newTargets
+
+            if changed:
+                pg['alertPolicy']['alertTargets'] = alertTargets
+                print('  updating %s' % pgname)
+                api('put', 'data-protect/protection-groups/%s' % pg.get('id', ''), pg, v=2)
+                if LAST_API_ERROR() != 'OK':
+                    print('  *** failed to update %s: %s' % (pgname, LAST_API_ERROR()))
+
         allEmails = ','.join([t.get('emailAddress', '') for t in alertTargets])
-        print('  %s (%s) -> %s' % (pgname, environment, allEmails))
+        if not modifying or changed:
+            print('  %s (%s) -> %s' % (pgname, environment, allEmails))
         report.append([clustername, pgname, environment, pg.get('id', ''), backupRunStatus, allEmails])
 
 # write report
